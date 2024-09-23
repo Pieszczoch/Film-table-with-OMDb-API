dcc480877d4bb57a8d627f3816bffa13808e2162
sebastian.kopacki <sebastian.kopacki5@gmail.com>
2024-09-23 16:58:43 +0200

    add config and request

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const API_URL = 'http://www.omdbapi.com/?apikey=[f33894c3]&';
+
+export const ONE_MINUTE_MS = 1000 * 60;
---
+++ src/lib/axios.ts
@@ -0,0 +1,122 @@
+import Axios from 'axios';
+import qs from 'qs';
+import { showNotification } from '@mantine/notifications';
+import { API_URL } from '@/consts';
+
+const axios = Axios.create({
+  baseURL: `${API_URL}/api`,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+  withCredentials: true,
+  paramsSerializer: (params: Record<string, unknown>) => {
+    return qs.stringify(params, { arrayFormat: 'brackets' });
+  },
+});
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 500) {
+      showNotification({
+        title: 'Error',
+        message:
+          error?.response?.message ?? error?.response?.data?.message ?? 'Internal server error',
+        color: 'red',
+      });
+      return;
+    }
+    if (error.response.status === 512) {
+      showNotification({
+        title: 'Warning',
+        message:
+          error?.response?.message ?? error?.response?.data?.message ?? 'Update completed but...',
+        color: 'orange',
+      });
+      throw error;
+    }
+    if (error.response.status === 422) {
+      try {
+        const errors = error.response.data.errors;
+        const messages = Object.values(errors).flat() as string[];
+        messages.forEach((message) => {
+          showNotification({
+            title: 'Error',
+            message: message,
+            color: 'red',
+          });
+        });
+      } catch (e) {
+        showNotification({
+          title: 'Error',
+          message: error.response.data.message ?? error.message,
+          color: 'red',
+        });
+      }
+      throw error;
+    }
+
+    showNotification({
+      title: 'Error',
+      message:
+        typeof error.response.data === 'string'
+          ? error.response.data
+          : (error.response.data.message ?? error.message),
+      color: 'red',
+    });
+    throw error;
+  }
+);
+
+const unauthAxios = Axios.create({
+  baseURL: `${API_URL}/api`,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+  withCredentials: true,
+});
+
+unauthAxios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 500) {
+      showNotification({
+        title: 'Error',
+        message: 'Internal server error ',
+        color: 'red',
+      });
+      return;
+    }
+    if (error.response.status === 401) {
+      return;
+    }
+    if (error.response.status === 422) {
+      try {
+        const errors = error.response.data.errors;
+
+        const messages = Object.values(errors).flat() as string[];
+        messages.forEach((message) => {
+          showNotification({
+            title: 'Error',
+            message: message,
+            color: 'red',
+          });
+        });
+      } catch (e) {
+        showNotification({
+          title: 'Error',
+          message: error.response.data.message ?? error.message,
+          color: 'red',
+        });
+      }
+      throw error;
+    }
+    showNotification({
+      title: 'Error',
+      message: error?.response?.data?.message ?? error?.message ?? 'Internal server error',
+      color: 'red',
+    });
+    throw error;
+  }
+);
+export { axios, unauthAxios };
